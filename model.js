030a5b080f6bdd42c71e302d14eb739c5e4c0b28
JessicaVitanza <103486794+JessicaVitanza@users.noreply.github.com>
2022-06-18 01:40:28 +0200

    css

---
+++ model.js
@@ -0,0 +1,15 @@
+const BASE_URL = 'https://628778b1e9494df61b39b038.mockapi.io/recipes';
+
+class Recipes{
+    constructor(name, scope, facility, time, ingredients, preparation, id){
+        this.name = name;
+        this.scope = scope;
+        this.facility = facility;
+        this.time = time;
+        this.ingredients = ingredients;
+        this.preparation = preparation;
+        this.id = id;
+    }
+
+
+}

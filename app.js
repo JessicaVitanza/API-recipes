f1f6cc1fd29f6343de7c15bb60809a64845facc8
JessicaVitanza <103486794+JessicaVitanza@users.noreply.github.com>
2022-06-27 23:11:03 +0200

    .

---
+++ app.js
@@ -0,0 +1,12 @@
+class Recipes{
+    constructor(name, scope, facility, time, ingredients, preparation, id){
+        this.name = name;
+        this.scope = scope;
+        this.facility = facility;
+        this.time = time;
+        this.ingredients = ingredients;
+        this.preparation = preparation;
+        this.id = id;
+    }
+}
+
